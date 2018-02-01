8224886d6247364c23170f444832f16b8d65696d
Taavi Kivisik <taavi.kivisik@gmail.com>
2018-02-01 22:02:26 +0100

    Added class structure, moved CLI parsing to func

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 ## Python Bits and Peace
 
 This repository is collecting useful Python bits for peace of mind.
+
+## What?
+
+* Command Line Parser - package: *argparse*
+* Code structuring - OOP
+* Logging - package: *logging*
+* Testing
--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 import argparse
 
-def main():
+class coin():
+    number_of_sides = 2
+
+    def __init__(self, name, value):
+        self.name = name
+        self.value = value
+
+    @classmethod
+    def display_currency_type(cls):
+        print("Coin's number of sides: {}".format(cls.number_of_sides))
+
+def parse_command_line():
     parser = argparse.ArgumentParser(description='This appears before usage.' ,epilog="This appears after usage. All arguments starting with a dash are considered optional.")
 
     parser.add_argument('integers', metavar='N', type=int, nargs='+',
@@ -14,6 +25,10 @@ def main():
     print args.accumulate(args.integers)
     print args.test
 
+
+def main():
+    c = coin("tc", 100)
+    c.display_currency_type()
 
 
 if __name__ == '__main__':
